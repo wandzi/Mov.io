0e79ccd46d730101674137b4f5e1f8e89030ce43
wandzi <wandzel.mateusz@yahoo.com>
2020-02-09 16:25:26 +0100

    Updates

---
+++ precache-manifest.205de064fc06da4c650c9f3af3696638.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ffde56ba70cf278396c1e6109eac469",
+    "url": "/search.io/index.html"
+  },
+  {
+    "revision": "699d58d825369ed2f8b6",
+    "url": "/search.io/static/css/main.3887b0d8.chunk.css"
+  },
+  {
+    "revision": "18417dbfa8382ff7c138",
+    "url": "/search.io/static/js/2.63d8aec5.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/search.io/static/js/2.63d8aec5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "699d58d825369ed2f8b6",
+    "url": "/search.io/static/js/main.98910a87.chunk.js"
+  },
+  {
+    "revision": "6b15b959b828146848ec",
+    "url": "/search.io/static/js/runtime-main.cccb490a.js"
+  }
+]);
